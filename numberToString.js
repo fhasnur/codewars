92056e9dd91f593cbf1f61a99ef0702ca500cd14
fhasnur <fandi.hasnur10@gmail.com>
2024-02-01 23:03:33 +0800

    add numberToString.js

---
+++ numberToString.js
@@ -0,0 +1,13 @@
+// function numberToString(num) {
+//   const result = "" + num;
+
+//   return result;
+// }
+
+// function numberToString(num) {
+//   return num.toString();
+// }
+
+const numberToString = num => num.toString();
+
+console.log(numberToString(123));
