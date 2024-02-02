d4396bf196482b4bbb4cbc0abece87cb1e9f6787
fhasnur <fandi.hasnur10@gmail.com>
2024-02-02 23:02:34 +0800

    day 8/366

---
+++ 2024-wpu-coding-challenge/8-countSheep.js
@@ -0,0 +1,19 @@
+/*
+  WPU Coding Challenge 2024
+  8/366
+  https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+*/
+
+// const countSheep = function (num) {
+//   let result = '';
+
+//   for (let i = 1; i <= num; i++) {
+//     result += `${i} sheep...`;
+//   }
+
+//   return result;
+// }
+
+const countSheep = (num) => [...Array(num)].map((_, i) => `${i + 1} sheep...`).join('');
+
+console.log(countSheep(3));
