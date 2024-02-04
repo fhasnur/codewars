0007aa313325e3c1f84e2cf9183157548b82b7f1
fhasnur <fandi.hasnur10@gmail.com>
2024-02-05 00:26:46 +0800

    day 10/366

---
+++ 2024-wpu-coding-challenge/10-stringToArray.js
@@ -0,0 +1,9 @@
+/*
+  WPU Coding Challenge 2024
+  10/366
+  https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+*/
+
+const stringToArray = (string) => string.split(' ');
+
+console.log("Fandi Meylwan Hasnur");
