87f24646973e43b80ea426bd43fc01566d7bfa00
fhasnur <fandi.hasnur10@gmail.com>
2024-02-06 23:43:20 +0800

    stringToNumber

---
+++ self/stringToNumber.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+// parseInt()
+// const stringToNumber = function (str) {
+//   return parseInt(str, 10);
+// }
+
+// Unary plus
+// const stringToNumber = str => +str;
+
+// Number()
+const stringToNumber = str => Number(str);
+
+console.log(stringToNumber("1234"));
