53bffdbe3fdd9dab1ad2aefbf0ca9811b69b52ea
fhasnur <fandi.hasnur10@gmail.com>
2024-02-09 20:51:25 +0800

    24-greet

---
+++ self/24-greet.js
@@ -0,0 +1,8 @@
+/*
+  Returning Strings
+  https://www.codewars.com/kata/55a70521798b14d4750000a4
+*/
+
+const greet = name => `Hello, ${name} how are you doing today?`;
+
+console.log(greet("Fandi"));
