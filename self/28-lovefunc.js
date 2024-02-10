8a03db015a0308389b09834ea3ec2ba31d8f189c
fhasnur <fandi.hasnur10@gmail.com>
2024-02-10 16:59:29 +0800

    28-lovefunc

---
+++ self/28-lovefunc.js
@@ -0,0 +1,8 @@
+/*
+  Opposites Attract
+  https://www.codewars.com/kata/555086d53eac039a2a000083
+*/
+
+const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2;
+
+console.log(lovefunc(1, 4));
