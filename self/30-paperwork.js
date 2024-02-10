4c01a803e28f0c9ccec33cefb7bf38a8977af9e7
fhasnur <fandi.hasnur10@gmail.com>
2024-02-10 17:16:14 +0800

    30-paperwork

---
+++ self/30-paperwork.js
@@ -0,0 +1,14 @@
+/*
+  Beginner Series #1 School Paperwork
+  https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+*/
+
+// function paperwork(n, m) {
+//   if (n < 0 || m < 0) return 0;
+
+//   return n * m;
+// }
+
+const paperwork = (n, m) => n < 0 || m < 0 ? 0 : n * m;
+
+console.log(paperwork(5, 5));
