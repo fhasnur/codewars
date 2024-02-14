8ad3e5d19f87fb5a5be0f586f454d848a41b3401
fhasnur <fandi.hasnur10@gmail.com>
2024-02-15 00:12:56 +0800

    38-simpleMultiplication

---
+++ self/38-simpleMultiplication.js
@@ -0,0 +1,13 @@
+/*
+  Simple multiplication
+  https://www.codewars.com/kata/583710ccaa6717322c000105
+*/
+
+// function simpleMultiplication(number) {
+//   if (number % 2 === 0) return number * 8;
+//   else return number * 9;
+// }
+
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9;
+
+console.log(simpleMultiplication(2));
