f2dba2d79008b6a69d8d3c0ddbc9c117d96cd186
fhasnur <fandi.hasnur10@gmail.com>
2024-02-16 23:40:39 +0800

    41-check

---
+++ self/41-check.js
@@ -0,0 +1,12 @@
+/*
+  You only need one - Beginner
+  https://www.codewars.com/kata/57cc975ed542d3148f00015b
+*/
+
+// function check(a, x) {
+//   return a.includes(x);
+// }
+
+const check = (a, x) => a.includes(x);
+
+console.log(check(['t', 'e', 's', 't'], 'e'));
