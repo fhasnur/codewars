749f433d8488f83c53ce9c6d3c1040d889f07dab
fhasnur <fandi.hasnur10@gmail.com>
2024-02-18 17:07:49 +0800

    48-greet

---
+++ self/48-greet.js
@@ -0,0 +1,8 @@
+/*
+  Grasshopper - Personalized Message
+  https://www.codewars.com/kata/5772da22b89313a4d50012f7
+*/
+
+const greet = (name, owner) => `Hello ${name === owner ? 'boss' : 'guest'}`;
+
+console.log(greet('Daniel', 'Daniel'));
