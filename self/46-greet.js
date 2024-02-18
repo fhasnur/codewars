82d23193daac01a3c484e315c910b1cca8862682
fhasnur <fandi.hasnur10@gmail.com>
2024-02-18 17:02:41 +0800

    47-areaOrPerimeter

--- self/46-greet.js
+++ self/46-greet.js
@@ -1,7 +1,4 @@
-/*
-  Jenny's secret message
-  https://www.codewars.com/kata/55225023e1be1ec8bc000390
-*/
+;
 
 // function greet(name) {
 //   if (name === "Johnny") return "Hello, my love!";
---
+++ self/47-areaOrPerimeter.js
@@ -0,0 +1,8 @@
+/*
+  Area or Perimeter
+  https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+*/
+
+const areaOrPerimeter = (l, w) => l === w ? l * w : 2 * (l + w);
+
+console.log(areaOrPerimeter(6, 10));
