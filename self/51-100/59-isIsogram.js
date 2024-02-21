82b1f8485170c0d8fa87a0319533e4a1c77c8ad1
fhasnur <fandi.hasnur10@gmail.com>
2024-02-21 23:06:59 +0800

    59-isIsogram

---
+++ self/51-100/59-isIsogram.js
@@ -0,0 +1,17 @@
+/*
+  Isograms
+  https://www.codewars.com/kata/54ba84be607a92aa900000f1
+*/
+
+function isIsogram(str) {
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) return false;
+    }
+  }
+  return true;
+}
+
+console.log(isIsogram("isogram"));
