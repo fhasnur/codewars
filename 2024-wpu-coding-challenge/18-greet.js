7a209619596675051c854b8826d9a0293b610459
fhasnur <fandi.hasnur10@gmail.com>
2024-02-21 23:10:51 +0800

    day 18/366

---
+++ 2024-wpu-coding-challenge/18-greet.js
@@ -0,0 +1,17 @@
+/*
+  WPU Coding Challenge 2024
+  18/366
+  https://www.codewars.com/kata/5772da22b89313a4d50012f7
+*/
+
+// function greet(name, owner) {
+//   if (name === owner) {
+//     return 'Hello boss';
+//   } else {
+//     return 'Hello guest';
+//   }
+// }
+
+const greet = (name, owner) => `Hello ${name === owner ? 'boss' : 'guest'}`;
+
+console.log(greet('Daniel', 'Daniel'));
