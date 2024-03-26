f6c7b67ee1d3b6a0355c0c225eaff87732883232
fhasnur <fandi.hasnur10@gmail.com>
2024-03-26 23:45:48 +0700

    75-isTriangle

---
+++ self/51-100/75-isTriangle.js
@@ -0,0 +1,8 @@
+/*
+  Is this a triangle?
+  https://www.codewars.com/kata/56606694ec01347ce800001b
+*/
+
+const isTriangle = (a, b, c) => a + b > c && a + c > b && b + c > a;
+
+console.log(isTriangle(1, 2, 2));
