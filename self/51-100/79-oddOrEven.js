dea4e3fb81882cd285e0d6ddbeac7b922e727b7b
fhasnur <fandi.hasnur10@gmail.com>
2024-03-29 23:18:51 +0700

    79-oddOrEven

---
+++ self/51-100/79-oddOrEven.js
@@ -0,0 +1,8 @@
+/*
+  Odd or Even?
+  https://www.codewars.com/kata/5949481f86420f59480000e7
+*/
+
+const oddOrEven = array => array.reduce((acc, curr) => acc + curr, 0) % 2 === 0 ? 'even' : 'odd';
+
+console.log(oddOrEven([0, 1, 5]));
