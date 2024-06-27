14f171866935b0919b0a34686d75870f2635905c
fhasnur <fandi.hasnur10@gmail.com>
2024-06-27 21:56:18 +0700

    2_multiply

---
+++ self/golang/2_multiply.go
@@ -0,0 +1,16 @@
+/*
+  Multiply
+  https://www.codewars.com/kata/50654ddff44f800200000004
+*/
+
+package main
+
+import "fmt"
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func main() {
+	fmt.Println(Multiply(2, 3))
+}
